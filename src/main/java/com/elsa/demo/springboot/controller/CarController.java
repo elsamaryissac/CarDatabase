f6df1e857dc5c334c486eceb13b916244ab28b96
unknown <elsajm15@gmail.com>
2022-05-13 23:17:05 +0530

    Today's topic was about Logging. Added the LOGGER.info msg line to all of the Car Controller's methods. Update as on 13th May 2022.

--- src/main/java/com/elsa/demo/springboot/controller/CarController.java
+++ src/main/java/com/elsa/demo/springboot/controller/CarController.java
@@ -2,6 +2,8 @@ package com.elsa.demo.springboot.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.elsa.demo.springboot.component.CarDatabase;
 import com.elsa.demo.springboot.model.Car;
 import com.elsa.demo.springboot.services.CarService;
 
@@ -21,17 +24,21 @@ import com.elsa.demo.springboot.services.CarService;
 @RequestMapping(value = "service/car", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
 public class CarController {
 	
+	private static final Logger LOGGER = LoggerFactory.getLogger(CarController.class);
+	
 	@Autowired
 	private CarService carService;
 	
 	@PostMapping("/add")
 	public ResponseEntity<Car> add(@RequestBody Car car) {
+		LOGGER.info("Car added to the database.");
 		car.setModified("elsa");
 		return new ResponseEntity<Car>(carService.addCar(car), HttpStatus.OK);
 	}
 	
 	@GetMapping("/getlist")
 	public ResponseEntity<?> getList(){
+		LOGGER.info("Updated the car list!");
 		carService.getCarList();
 		//return new ResponseEntity<>("List is updated!", HttpStatus.OK);
 		return new ResponseEntity<List<Car>>(carService.getCarList(), HttpStatus.OK);
@@ -41,7 +48,7 @@ public class CarController {
 	@GetMapping("/get")
 	public ResponseEntity<?> get(@RequestParam Integer id){
 		Car car = carService.get(id);
-		//return new ResponseEntity<>("List is updated!", HttpStatus.OK);
+		LOGGER.info("Car available with this id is : "+car.getName());
 		return new ResponseEntity<Car>(car, car==null?HttpStatus.NO_CONTENT: HttpStatus.OK);
 		
 	}
@@ -49,7 +56,7 @@ public class CarController {
 	@GetMapping("/getbyid/{id}")
 	public ResponseEntity<?> getbyid(@PathVariable(required=true) Integer id){
 		Car car = carService.get(id);
-		//return new ResponseEntity<>("List is updated!", HttpStatus.OK);
+		LOGGER.info("Car available with this id is : "+car.getName());
 		return new ResponseEntity<Car>(car, car==null?HttpStatus.NO_CONTENT: HttpStatus.OK);
 		
 	}
@@ -57,6 +64,7 @@ public class CarController {
 	@GetMapping("/getcompany")
 	public ResponseEntity<?> get(@RequestParam String company){
 		Car car = carService.get(company);
+		LOGGER.info("Car Company is available : "+car.getName());
 		return new ResponseEntity<Car>(car, car==null?HttpStatus.NO_CONTENT: HttpStatus.OK);
 		
 	}
@@ -64,7 +72,7 @@ public class CarController {
 	@GetMapping("/getbycompany/{company}")
 	public ResponseEntity<?> getbycompany(@PathVariable(required=true) String company){
 		Car car = carService.get(company);
-		//return new ResponseEntity<>("List is updated!", HttpStatus.OK);
+		LOGGER.info("Car Company is available : "+car.getName());
 		return new ResponseEntity<Car>(car, car==null?HttpStatus.NO_CONTENT: HttpStatus.OK);
 		
 	}
